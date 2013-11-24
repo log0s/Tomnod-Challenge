189378770bd5169f327826dcd3b24d82e35f1f4e
log0s <JewishRhino@gmail.com>
2013-11-24 15:58:39 -0700

    Added functionality for displaying satellite pictures

--- tomnod.js
+++ tomnod.js
@@ -1,20 +1,53 @@
 var app = {
 	init: function() {
-		google.maps.event.addListener(map, 'dblclick', function(ev) {
+		$('#satPic').hide();
+
+		google.maps.event.addListener(map, 'click', function(ev) {
 			if (app.vars.getImage)
 				app.getCoords(ev);
 			else
 				return;
 
 		});
+
+		$('#getImage').click(app.activateRetrieve);
+	},
+
+	activateRetrieve: function() {
+		app.vars.getImage = true;
+
+		app.mapCursor('default');
 	},
 
 	getCoords: function(ev) {
-		console.log('lat: ' + ev.latLng.lat() + 'lng: ' + ev.latLng.lng() );
+		app.vars.coords.lat = ev.latLng.lat();
+		app.vars.coords.lng = ev.latLng.lng();
+
+		app.setImage();
+	},
+
+	setImage: function() {
+		var $satPic = $('#satPic');
+
+		app.mapCursor('progress');
+		$satPic.css('background-image', 'url("http://dev1.tomnod.com/chip_api/chip/lat/' + app.vars.coords.lat + '/lng/' + app.vars.coords.lng + '")');
+
+		//Using setTimeout to allow the image to load before showing it
+		setTimeout(function() { 
+			$satPic.show('scale');
+			app.mapCursor('-webkit-grab');
+		}, 7000);
+
+		app.vars.getImage = false;
+	},
+
+	mapCursor: function(pointer) {
+		map.setOptions( {draggableCursor: pointer} );
 	},
 
 	vars: {
-		getImage: false
+		getImage: false,
+		coords: { lat: 0, lng: 0 }
 	}
 };
 
